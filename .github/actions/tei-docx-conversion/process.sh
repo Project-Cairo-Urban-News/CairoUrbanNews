960da9c8dfb480366a50d7e086382af90e1ae07c
Hugh Cayless <philomousos@gmail.com>
2020-07-08 08:30:55 -0400

    Using right directory.

--- .github/actions/tei-docx-conversion/process.sh
+++ .github/actions/tei-docx-conversion/process.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for f in `ls /opt/CairoUrbanNews/docx/*.docx`
+for f in `ls /home/github/docx/*.docx`
 do
   OUT1 = $(echo $f | sed 's/\([[:digit:]]*\).docx/\1.xml/')
   OUT2 = $(echo $OUT1 | sed 's/docx\//')
@@ -8,5 +8,4 @@ do
     /opt/stylesheets/bin/docxtotei $f $OUT1
   fi
   saxon -s:$OUT1 -xsl:/opt/CairoUrbanNews/xslt/process-new.xsl -o:$OUT2
-  cp $OUT2 /github/home/$(echo $OUT2 | sed 's/\/opt\/CairoUrbanNews\//')
 done
