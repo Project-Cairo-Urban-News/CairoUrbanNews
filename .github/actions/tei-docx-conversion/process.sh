ad13376fdb11a709a21e2de121e0ae38468ac63a
Hugh Cayless <philomousos@gmail.com>
2020-07-08 11:53:32 -0400

    Added path filter to workflow.

--- .github/actions/tei-docx-conversion/process.sh
+++ .github/actions/tei-docx-conversion/process.sh
@@ -4,7 +4,7 @@ for f in `ls docx/*.docx`
 do
   OUT1=$(echo $f | sed 's/\([[:digit:]]*\).docx/\1.xml/')
   OUT2=$(echo $OUT1 | sed 's/docx\///')
-  if [ ! -f $OUT1 ]; then
+  if [ ! -f $OUT2 ]; then
     /opt/stylesheets/bin/docxtotei $f $OUT1
   fi
   saxon -s:$OUT1 -xsl:xslt/process-new.xsl -o:$OUT2
