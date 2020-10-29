40e8c3b3984861d3c014a62af57dbd10f6dd1553
Hugh Cayless <philomousos@gmail.com>
2020-10-29 10:42:10 -0400

    Slimmed-down Dockerfile and troubleshooting script.

--- .github/actions/update-header/process.sh
+++ .github/actions/update-header/process.sh
@@ -3,8 +3,10 @@
 REGEX="[[:digit:]]{4}\.xml"
 for f in `ls *.xml`
 do
+  echo "Checking $f"
   if [[ $f =~ $REGEX ]]
   then
+    echo "Converting $f"
     saxon -s:"$f" -xsl:xslt/apply-master.xsl -o:"$f"
   fi
 done
