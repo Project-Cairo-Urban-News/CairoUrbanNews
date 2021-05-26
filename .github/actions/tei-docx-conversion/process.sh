078144ddbf23b2a2f74330f857f8e9c6178dc659
Hugh Cayless <philomousos@gmail.com>
2021-05-26 10:58:38 -0400

    Fixing directory path.

--- .github/actions/tei-docx-conversion/process.sh
+++ .github/actions/tei-docx-conversion/process.sh
@@ -4,7 +4,7 @@ convert() {
   for f in `ls docx/$1/*.docx`
   do
     OUT1=$(echo $f | sed 's/\([[:digit:]]*\).docx/\1.xml/')
-    OUT2=$(echo $OUT1 | sed "s/docx\//$1\//")
+    OUT2=$(echo $OUT1 | sed "s/docx\//articles\//")
     if [ ! -f "$OUT2" ]; then
       /opt/stylesheets/bin/docxtotei $f "$OUT1"
       saxon -s:"$OUT1" -xsl:xslt/process-$1.xsl -o:"$OUT2"
